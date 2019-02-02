968cf0cf55bb252eb6db100383e78f9de870682a
Michael McClurg <michael.mcclurg@outlook.com>
2019-02-01 21:59:18 -0600

    Document the accumulator array and add todos

--- accum.py
+++ accum.py
@@ -3,7 +3,27 @@ import glob
 import numpy as np
 import sys
 import math
-        
+
+"""
+Generates the Hough accumulator array for the given image.
+The lines are defined by the point in the image (ie, the pixel indices of the image)
+and the angle from the vertical, clockwise.
+
+The array is in a polar coordinate system based on the normal vector to the line.
+The vertical (first) index is the magnitude of that vector.
+The horizontal (second) index is the polar angle of the vector, clockwise from the horizontal.
+
+TODO: change angle from going up to 2*pi. A line at theta=pi/2 is the same as one at 3*pi/2.
+
+TODO: double check how the angle is handled. I don't know for sure if this works as expected.
+According to the openCV spec (which I think we should stick to), if the line passes below the
+origin (when it intersects the vertical axis), they take the radius as positive. If it passes
+above the origin, the radius is negative.
+This is definitely not implemented correctly here. We don't have negative radius and angle goes
+0 to 2*pi, unlike theirs.
+Alternatively, we could define the angle from 0 to 2*pi, instead of pi. It would differ from
+their spec, but as long as we're consistent and know that, we should be ok.
+"""
 def accumulate(image, theta_buckets, rho_buckets):
     
     accum = np.zeros((rho_buckets, theta_buckets))
