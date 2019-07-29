6e18ae9094c75888e018f11287b09a34fca6cc35
Thong Vo <vo4@stolaf.edu>
2019-07-29 03:23:33 -0500

    Upgrade QR code recognition => Working

--- rectangle_detection.py
+++ rectangle_detection.py
@@ -5,14 +5,32 @@ import argparse
 import random as rng
 rng.seed(12345)
 
+# Size of final image
 HEIGHT = 1200
 WIDTH = 1200
-area_threshold = 3
 
+area_threshold = 3 # ignore contour with area smaller than that
+
+quadrant_received = [0, 0, 0, 0]
+# bottom right, bottom left, top right, top left
+# 0: not received yet, 1 received
+
+
+Position = {"bottom right": 0, "bottom left": 1, "top right": 2, "top left": 3}
 def distance(point1, point2):
     return (point1[0] - point2[0])**2 + (point1[1] - point2[1])**2
 
-def thresh_callback(val, src, position):
+def decode_QR(image):
+    from pyzbar.pyzbar import decode
+    from PIL import Image
+    # print( decode(Image.open(image)))
+    try:
+        print( decode(Image.open(image))[0].data)
+        # print( decode(Image.open("qr/Easy/result.jpg"))[0].data)
+    except IndexError:
+        print("Not enough data")
+
+def extract_and_collect_qr(val, src, position, output):
     # Convert image to gray and blur it
     src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
     src_gray = cv.blur(src_gray, (3,3))
@@ -22,30 +40,6 @@ def thresh_callback(val, src, position):
     canny_output = cv.Canny(src_gray, threshold, threshold * 2)
     # Find contours
     contours, _ = cv.findContours(canny_output, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    
-    # hull_list = []
-    # for i in range(len(contours)):
-    #     hull = cv.convexHull(contours[i])
-    #     hull_list.append(hull)
-
-    # # Draw contours + hull results
-    # drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
-    # for i in range(len(contours)):
-    #     color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
-    #     # cv.drawContours(drawing, contours, i, color)
-    #     cv.drawContours(drawing, hull_list, i, color)
-    # # Show in a window
-    # cv.imshow('Hulls', drawing)
-
-    # # Draw hulls result
-    # # drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
-    # drawing = src
-    # for i in range(len(contours)):
-    #     color = (256,0,0)
-    #     cv.drawContours(drawing, contours, i, color)
-    #     # cv.drawContours(drawing, hull_list, i, color)
-    # # Show in a window
-    # cv.imshow('Contours', drawing)
 
     # Find the convex hull object for all contours
     hull_list = []
@@ -70,16 +64,6 @@ def thresh_callback(val, src, position):
 
     width_src, height_src, channels_src = src.shape
 
-    # top = hull[0][0][1]
-    # down = hull[0][0][1]
-    # left = hull[0][0][0]
-    # right = hull[0][0][0]
-    # for point in hull:
-    #     top = max(top, point[0][1])
-    #     down = min(down, point[0][1])
-    #     left = min(left, point[0][0])
-    #     right = max(right, point[0][0])
-
     # Extract corner of the hull
     corner_top_left = 0
     for i in range(1, len(hull) ):
@@ -98,65 +82,109 @@ def thresh_callback(val, src, position):
         if distance(hull[i][0], [width_src, height_src]) < distance(hull[corner_bottom_right][0], [width_src, height_src]):
             corner_bottom_right = i
 
-    if position == "bottom right":
+    if position == 0:
         # Find the point that will map into the middle of result
-        pts1 = np.float32([hull[corner_top_left][0], hull[corner_bottom_left][0], hull[corner_top_right][0]])
-        pts2 = np.float32([ [600,600], [600,1200], [1200,600] ])
-        M = cv.getAffineTransform(pts1,pts2)
+        # pts1 = np.float32([hull[corner_top_left][0], hull[corner_bottom_left][0], hull[corner_top_right][0]])
+        # pts2 = np.float32([ [600,600], [600,1200], [1200,600] ])
+        # M = cv.getAffineTransform(pts1,pts2)
 
-        dst = cv.warpAffine(src, M, (height_src,width_src))
+        # dst = cv.warpAffine(src, M, (height_src,width_src))
         # dst = cv.warpPerspective(src,M,(300,300))
-        result = cv.imread("qr/result.jpg")
+        pts1 = np.float32([hull[corner_top_left][0], hull[corner_bottom_left][0], hull[corner_top_right][0], hull[corner_bottom_right][0]])
+        pts2 = np.float32([[600,600], [600,1200], [1200,600], [1200,1200]])
+
+        M = cv.getPerspectiveTransform(pts1,pts2)
+
+        dst = cv.warpPerspective(src,M,(1200,1200))
+
+        result = cv.imread(output)
         result[600:1200, 600:1200] = dst[600:1200, 600:1200]
-        cv.imwrite("qr/result.jpg", result)
+        cv.imwrite(output, result)
 
-    elif position == "bottom left":
+        quadrant_received[0] = 1
+
+    elif position == 1:
+        # # Find the point that will map into the middle of result
+        # pts1 = np.float32([hull[corner_top_right][0], hull[corner_bottom_right][0], hull[corner_top_left][0]])
+        # pts2 = np.float32([ [600,600], [600,1200], [0,600] ])
+
+        # M = cv.getAffineTransform(pts1,pts2)
+
+        # dst = cv.warpAffine(src, M, (height_src,width_src))
+        # # dst = cv.warpPerspective(src,M,(300,300))
+
+        # result = cv.imread(output)
+        # # result = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
+        # result[600:1200, 0:600] = dst[600:1200, 0:600]
+        # cv.imwrite(output, result)
+
+        pts1 = np.float32([hull[corner_top_right][0], hull[corner_bottom_right][0], hull[corner_top_left][0], hull[corner_bottom_left][0]])
+        pts2 = np.float32([[600,600], [600,1200], [0,600], [0,1200]])
+
+        M = cv.getPerspectiveTransform(pts1,pts2)
+
+        dst = cv.warpPerspective(src,M,(1200,1200))
+
+        result = cv.imread(output)
+        result[600:1200, 0:600] = dst[600:1200, 0:600 ]
+        cv.imwrite(output, result)
+
+        quadrant_received[1] = 1
+
+    elif position == 2:
         # Find the point that will map into the middle of result
-        pts1 = np.float32([hull[corner_top_right][0], hull[corner_bottom_right][0], hull[corner_top_left][0]])
-        pts2 = np.float32([ [600,600], [600,1200], [0,600] ])
+        # pts1 = np.float32([hull[corner_bottom_left][0], hull[corner_bottom_right][0], hull[corner_top_left][0]])
+        # pts2 = np.float32([ [600,600], [1200,600], [600,0] ])
 
-        M = cv.getAffineTransform(pts1,pts2)
+        # M = cv.getAffineTransform(pts1,pts2)
+        # dst = cv.warpAffine(src, M, (height_src,width_src))
 
-        dst = cv.warpAffine(src, M, (height_src,width_src))
-        # dst = cv.warpPerspective(src,M,(300,300))
+        # result = cv.imread(output)
+        # result[0:600, 600:1200] = dst[0:600, 600:1200]
+        # cv.imwrite(output, result)
 
-        result = cv.imread("qr/result.jpg")
-        # result = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
-        result[600:1200, 0:600] = dst[600:1200, 0:600]
-        cv.imwrite("qr/result.jpg", result)
+        pts1 = np.float32([hull[corner_bottom_left][0], hull[corner_bottom_right][0], hull[corner_top_left][0], hull[corner_top_right][0]])
+        pts2 = np.float32([[600,600], [1200,600], [600,0], [1200, 0]])
 
+        M = cv.getPerspectiveTransform(pts1,pts2)
 
-    elif position == "top right":
-        # Find the point that will map into the middle of result
-        pts1 = np.float32([hull[corner_bottom_left][0], hull[corner_bottom_right][0], hull[corner_top_left][0]])
-        pts2 = np.float32([ [600,600], [1200,600], [600,0] ])
+        dst = cv.warpPerspective(src,M,(1200,1200))
 
-        M = cv.getAffineTransform(pts1,pts2)
-        dst = cv.warpAffine(src, M, (height_src,width_src))
-
-        result = cv.imread("qr/result.jpg")
+        result = cv.imread(output)
         result[0:600, 600:1200] = dst[0:600, 600:1200]
-        cv.imwrite("qr/result.jpg", result)
+        cv.imwrite(output, result)
 
-    else: # top left
-        pts1 = np.float32([hull[corner_bottom_right][0], hull[corner_top_right][0], hull[corner_bottom_left][0]])
-        pts2 = np.float32([ [600,600], [600,0], [0,600] ])
+        quadrant_received[2] = 1
 
-        M = cv.getAffineTransform(pts1,pts2)
-        dst = cv.warpAffine(src, M, (height_src,width_src))
+    else: # 3: top left
+        # pts1 = np.float32([hull[corner_bottom_right][0], hull[corner_top_right][0], hull[corner_bottom_left][0]])
+        # pts2 = np.float32([ [600,600], [600,0], [0,600] ])
 
-        result = cv.imread("qr/result.jpg")
+        # M = cv.getAffineTransform(pts1,pts2)
+        # dst = cv.warpAffine(src, M, (height_src,width_src))
+
+        # result = cv.imread(output)
+        # result[0:600, 0:600] = dst[0:600, 0:600]
+        # cv.imwrite(output, result)
+
+        pts1 = np.float32([hull[corner_bottom_right][0], hull[corner_top_right][0], hull[corner_bottom_left][0], hull[corner_top_left][0]])
+        pts2 = np.float32([[600,600], [600,0], [0,600], [0,0]])
+
+        M = cv.getPerspectiveTransform(pts1,pts2)
+
+        dst = cv.warpPerspective(src,M,(1200,1200))
+
+        result = cv.imread(output)
+        # result = cv.imread("qr/Ken/result_tmp.jpg")
         result[0:600, 0:600] = dst[0:600, 0:600]
-        cv.imwrite("qr/result.jpg", result)
+        cv.imwrite(output, result)
 
+        quadrant_received[3] = 1
 
-def decode_QR(image):
-    from pyzbar.pyzbar import decode
-    from PIL import Image
-    # print( decode(Image.open(image)))
-    print( decode(Image.open(image))[0].data)
+    # if 0 not in quadrant_received:
+    decode_QR(output)
 
-def process_image(image):
+def process_image(image, position):
     # Load source image
     src = cv.imread(cv.samples.findFile(image))
     src = cv.resize(src, (int(WIDTH),int(HEIGHT) ) )
@@ -166,23 +194,23 @@ def process_image(image):
         exit(0)
 
     # decode_QR(image)
-    # # Create Window
-    # source_window = 'Source'
-    # cv.namedWindow(source_window)
-    # cv.imshow(source_window, src)
     max_thresh = 255
     # thresh = 100 # initial threshold
     thresh = 130 # initial threshold
-    # cv.createTrackbar('Canny thresh:', source_window, thresh, max_thresh, thresh_callback)
-    thresh_callback(thresh, src, "bottom left" )
-    # 1: bottom right, 2: bottom left, 3: top left
-    # cv.waitKey()
+    extract_and_collect_qr(thresh, src, position, "qr/Ken/result_tmp.jpg")
 
 
 parser = argparse.ArgumentParser(description='Code for Convex Hull tutorial.')
 parser.add_argument('--input', help='Path to input image.', default='qr/qr1.png')
+parser.add_argument('--position', help='Position of input image. bottom right:0, bottom left: 1, top right: 2, top left: 3', default='0')
+parser.add_argument('--output', help='Output file', default='result.jpg')
+
+# print( parser.parse_args())
 args = parser.parse_args()
-process_image(args.input)
+image = args.input
+position = args.position
+output = args.output
+process_image(image, int(position) )
 # decode_QR(args.input)
 cv.waitKey()
 
